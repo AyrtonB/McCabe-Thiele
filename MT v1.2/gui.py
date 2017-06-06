70a73dd779411adc02d02570b149214bc8e70913
AyrtonB <ayrtonbourn@outlook.com>
2017-06-06 12:56:55 +0100

    Add files via upload

--- MT v1.2/gui.py
+++ MT v1.2/gui.py
@@ -1,49 +1,68 @@
 import tkinter as tk
 from tkinter import ttk
 
-from PIL import ImageTk as imtk, Image as im
+from PIL import ImageTk as imtk #, Image as im
+import csv
 
+
+#AC_LK = np.array([4.02232,1206.53,220.291]) # (AC_LK)
+#AC_HK = np.array([4.0854,1348.77,219.976]) # (AC_HK)
+
+AC_LK_A = 4.02232
+AC_LK_B = 1206.53
+AC_LK_C = 220.291 # (AC_LK)
+
+AC_HK_A = 4.0854
+AC_HK_B = 1348.77
+AC_HK_C = 219.976 # (AC_HK)
+
+N_Size = 1001 # (N_Size), 1001 = 0.1% accuracy for xA
+Max_Pl = 30 # (Max_Pl) Maximum number of plates
 
 Large_Font = ("Verdana", 12)
 Label_Font = ("Verdana", 10)
 
+def Decimalise(x,dec):   
+    a = pow(10,dec)
+    x = int(x*a)/a
+    return x
 
 def To_Print(self):
-    
-    import Eqm as Eqm
-    
-    def Outputs(self, x):
+                      
+    if Check_Inputs_Entered(self) == 'y':
         
-        if x == 'Num_Plates':
-            x = Eqm.Outputs('Num_Plates')
-            
-        elif x == 'R':
-            x = Eqm.Outputs(self,'R')
-            
-        elif x == 'Rmin':
-            x = Eqm.Outputs('Rmin')
-                    
-        elif x == 'x_q_TOL':
-            x = float(Eqm.Outputs('x_q_TOL'))
-    
-        return x
-    
-    #Num_Plates = Outputs('Num_Plates')
-    #Rmin = Outputs('Rmin')
-    R = Outputs(self, 'R')
-    #x_q_TOL = Outputs('x_q_TOL')
-    
-    if Inputs_Entered(self) == 'y':
-        xf = Extract_Inputs(self,'xf')
+        Write_Inputs_To_CSV(self)
+        
+        import Eqm
+        
+        Eqm.Pre_Output()
+        
+        Rmin = Decimalise(Eqm.Outputs('Rmin'),3)
+        R = Decimalise(Eqm.Outputs('R'),3)
+        Num_Plates = Eqm.Outputs('Num_Plates')
+        x_q_TOL = Decimalise(Eqm.Outputs('x_q_TOL'),3)
+        
         xd = Extract_Inputs(self,'xd')
-        print('xf = ', xf)
+        xf = Extract_Inputs(self,'xf')
+        xb = Extract_Inputs(self,'xb')
+        
         print('xd = ', xd)
+        print('xf = ', xf)
+        print('xb = ', xb)
+        print(' ')
+        print('Rmin = ', Rmin)
         print('R = ', R)
+        print('Num_Plates = ', Num_Plates)
+        print('x_q_TOL = ', x_q_TOL)
+        
+        del Eqm
     
     else:
         print('Enter All Inputs')
+        
     
-def Inputs_Entered(self):
+    
+def Check_Inputs_Entered(self):
     
     Entered = 'y'
     
@@ -99,6 +118,40 @@ def Extract_Inputs(self,x): # Enter inputs from values typed in
       
       return x
 
+def Write_Inputs_To_CSV(self):
+    
+    xf = Extract_Inputs(self,'xf')
+    xd = Extract_Inputs(self,'xd')
+    xb = Extract_Inputs(self,'xb')
+    
+    P_Tot = Extract_Inputs(self,'P_Tot')
+    q = Extract_Inputs(self,'q')
+    Rf = Extract_Inputs(self,'Rf')
+    
+    with open('Inputs.csv', 'w', newline='') as csvfile:
+        
+        filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+        filewriter.writerow(['Variable', 'Value'])
+        
+        filewriter.writerow(['xf', xf])
+        filewriter.writerow(['xd', xd])
+        filewriter.writerow(['xb', xb])
+        
+        filewriter.writerow(['P_Tot', P_Tot])
+        filewriter.writerow(['q', q])
+        filewriter.writerow(['Rf', Rf])
+        
+        filewriter.writerow(['AC_LK_A', AC_LK_A])
+        filewriter.writerow(['AC_LK_B', AC_LK_B])
+        filewriter.writerow(['AC_LK_C', AC_LK_C])
+        filewriter.writerow(['AC_HK_A', AC_HK_A])
+        filewriter.writerow(['AC_HK_B', AC_HK_B])
+        filewriter.writerow(['AC_HK_C', AC_HK_C])
+        
+        filewriter.writerow(['N_Size', N_Size])
+        filewriter.writerow(['Max_Pl', Max_Pl])
 
 class ChemEngApp(tk.Tk):
 
@@ -106,7 +159,7 @@ class ChemEngApp(tk.Tk):
         
         tk.Tk.__init__(self, *args, **kwargs)
 
-        #tk.Tk.iconbitmap(self,default='clienticon.ico')
+        tk.Tk.iconbitmap(self,default='CES.ico')
         tk.Tk.wm_title(self, "ChemEng")
 
         container = tk.Frame(self)
@@ -182,16 +235,24 @@ class StartPage(tk.Frame):
         self.e_Rf.grid(row=7, column=1)       
         
         
-        self.b_Run = ttk.Button(self, text="Click to Run", command=lambda: To_Print(self))
-        self.b_Run.grid(row=8, column=0, columnspan = 2)   
+        self.b_Run = ttk.Button(self, text="Click to Run", command=lambda: RUN(self))
+        self.b_Run.grid(row=8, column=0, columnspan = 2)  
         
-        self.img = imtk.PhotoImage(im.open("x.png"))
+        self.img = imtk.PhotoImage(file="MT.png")
         self.panel = tk.Label(self, image = self.img)
         self.panel.grid(row = 0, column = 2, rowspan = 10) 
     
         self.b_pg1 = ttk.Button(self, text="Visit Page 1",
                             command=lambda: controller.show_frame(PageOne))
         self.b_pg1.grid(row = 9, columnspan = 2)
+        
+        def RUN(self):
+        
+            To_Print(self)
+            
+            self.img = imtk.PhotoImage(file="MT.png")
+            self.panel = tk.Label(self, image = self.img)
+            self.panel.grid(row = 0, column = 2, rowspan = 10) 
 
     
 class PageOne(tk.Frame):
